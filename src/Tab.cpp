de39ff692157fb7f7bdf03ba4511f9ddd227c06b
Aubane <aubane.nourry@epitech.eu>
2024-11-18 18:06:52 +0100

    :sparkles: Add tab first implementation

--- src/Tab.cpp
+++ src/Tab.cpp
@@ -6,3 +6,67 @@
 */
 
 #include "Tab.hpp"
+
+#include <utility>
+
+Tab::Tab(std::string tab_name, const std::string& icon_path, const bool is_removable, const std::vector<IUIElement*>& tab_content)
+    : name(std::move(tab_name)), removable(is_removable)
+{
+    icon = FileManager::loadTexture(icon_path);
+    content = tab_content;
+}
+
+std::string Tab::get_name() const
+{
+    return name;
+}
+
+SDL_Texture* Tab::get_icon() const
+{
+    return icon;
+}
+
+bool Tab::is_removable() const
+{
+    return removable;
+}
+
+std::vector<IUIElement*> Tab::get_content() const
+{
+    return content;
+}
+
+void Tab::set_name(const std::string& new_name)
+{
+    name = new_name;
+}
+
+void Tab::set_content(const std::vector<IUIElement*>& new_content)
+{
+    content = new_content;
+}
+
+void Tab::set_removable(const bool is_removable)
+{
+    removable = is_removable;
+}
+
+void Tab::render(SDL_Renderer* renderer)
+{
+    constexpr SDL_Rect tabRect = {0, 0, 200, 25};
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &tabRect);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderDrawRect(renderer, &tabRect);
+    SDL_RenderCopy(renderer, icon, nullptr, &tabRect);
+
+    for (const auto& element : content) {
+        element->render(renderer);
+    }
+}
+
+void Tab::add_content(IUIElement* new_content)
+{
+    content.push_back(new_content);
+}
+
--- src/Tab.hpp
+++ src/Tab.hpp
@@ -10,25 +10,31 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
 #include <SDL2/SDL.h>
+
+#include "IUIElement.hpp"
+#include "FileManager.hpp"
 
 class Tab {
 public:
-    Tab(const std::string& tab_name, const std::string& icon_path, bool is_removable, SDL_Texture* tab_content);
-    ~Tab();
-    std::string get_name() const;
-    SDL_Texture* get_icon() const;
-    bool is_removable() const;
-    SDL_Texture* get_content() const;
+    Tab(std::string  tab_name, const std::string& icon_path, bool is_removable, const std::vector<IUIElement*>& tab_content = {});
+    ~Tab() = default;
+    [[nodiscard]] std::string get_name() const;
+    [[nodiscard]] SDL_Texture* get_icon() const;
+    [[nodiscard]] bool is_removable() const;
+    [[nodiscard]] std::vector<IUIElement*> get_content() const;
     void set_name(const std::string& new_name);
-    void set_content(SDL_Texture* new_content);
+    void set_content(const std::vector<IUIElement*>& new_content);
     void set_removable(bool is_removable);
+    void render(SDL_Renderer* renderer);
+    void add_content(IUIElement *new_content);
 
 private:
     std::string name;
     SDL_Texture* icon;
     bool removable;
-    SDL_Texture* content;
+    std::vector<IUIElement*> content;
 };
 
 #endif // TAB_HPP
